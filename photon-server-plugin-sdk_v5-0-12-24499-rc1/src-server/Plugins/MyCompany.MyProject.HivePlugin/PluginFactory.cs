a33c73a96e53103b318df647afc5a5a716466347
Dong-yeon <ehddus5712@gmail.com>
2022-02-10 18:01:47 +0900

    2022/02/10

---
+++ photon-server-plugin-sdk_v5-0-12-24499-rc1/src-server/Plugins/MyCompany.MyProject.HivePlugin/PluginFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.Hive.Plugin;
+
+namespace MyCompany.MyProject.HivePlugin
+{
+    public class PluginFactory : PluginBase
+    {
+        public override void OnCreateGame(ICreateGameCallInfo info)
+        {
+            base.OnCreateGame(info);
+        }
+    }
+}
